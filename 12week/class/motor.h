a91918d8c01cdbba7778d1a64c5d3946058d5be4
kwonhojun22 <163806960+kwonhojun22@users.noreply.github.com>
2024-05-22 19:42:37 +0900

    Create motor.h

---
+++ 12week/class/motor.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "car.h"
+class motor : public car {
+private:
+	int spareWheels;
+	int helmets;
+public:
+	void klaxon(int);
+	void setSparewheels(int);
+	int getSparewheels();
+	float getTotalPrice();
+};

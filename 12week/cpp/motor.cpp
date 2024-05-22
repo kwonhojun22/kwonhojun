1b179de7b2d78eedec654830fdad9ce0ab949837
kwonhojun22 <163806960+kwonhojun22@users.noreply.github.com>
2024-05-22 19:43:42 +0900

    Create motor.cpp

---
+++ 12week/cpp/motor.cpp
@@ -0,0 +1,16 @@
+#include "motor.h"
+#include <iostream>
+void motor::setSparewheels(int ii) {
+	spareWheels = ii;
+}
+int motor::getSparewheels() {
+	return spareWheels;
+};
+float motor::getTotalPrice() {
+	return (this->wheels * 100.0 + this->price);
+}
+void motor::klaxon(int ii) {
+	for (int i = 0; i < ii; i++) {
+		std::cout << "BagaBaga\n";
+	}
+}
